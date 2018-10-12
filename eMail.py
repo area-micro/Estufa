9af2bde320042a357d6726145a567a1336f97f12
area-micro <cmmattan@gmail.com>
2018-10-12 01:11:03 -0300

    prueba

---
+++ eMail.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+# import necessary packages
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.parser import Parser
+import smtplib
+import poplib
+from time import sleep
+
+#datos
+password = 'AreaMicro1'
+origen = 'estufa.unsl@gmail.com'
+Lista = ["Foto","foto","FOTO","Captura","captura","CAPTURA","Imagen","imagen",
+	"IMAGEN","Enviar foto","enviar foto", "Enviar Foto", "enviar Foto",
+	"ENVIAR FOTO"]
+
+# Se establece conexion con el servidor pop de gmail para ver si hay emails nuevos.
+
+m = poplib.POP3_SSL('pop.gmail.com',995)
+m.user(origen)
+m.pass_(password)
+
+#Me fijo la cantidad de nuevos mensajes
+numero = len(m.list()[1])
+print numero
+#Si numero es distinto de cero es por que hay mensajes nuevos.
+j = 0
+if numero != 0:
+	#Se toma la foto
+
+	#Se establece conexion con smtp.gmail.com
+	server = smtplib.SMTP('smtp.gmail.com',587)
+	server.ehlo()
+	server.starttls()
+	server.ehlo()
+
+	msg = MIMEMultipart()
+	msg["From"] = origen
+	msg["subject"] = "Foto Cultivo Bacteriano"
+
+	server.login(msg["From"],password)
+
+	#Analizo los mensajes
+	for i in range(numero):
+		j = j+1
+		# Se lee el mensaje
+		response, headerLines, bytes = m.retr(i+1)
+
+		#Se une todo en un solo string
+		mensaje='\n'.join(headerLines)
+
+		#Se parsea el mensaje
+		p = Parser()
+	 	email = p.parsestr(mensaje)
+
+		#Aqui ya se tienen los datos de forma entendible
+#		print "Mail %d" %j
+#		print "From: "+email["From"]
+#		print "To: "+email["To"]
+#	 	print "Subject: "+email["Subject"]
+
+		if email["Subject"] in Lista:
+
+#		msg = MIMEMultipart()
+
+
+#		message = ""
+
+		# setup the parameters of the message
+
+#		msg['From'] = origen
+			msg['To'] = email["From"]
+#		msg['Subject'] = "Foto Cultivo Bacteriano"
+
+		#add message body
+		#msg.attach(MIMEText(message, 'plain'))
+
+
+		#Adjunto la foto del cultivo bacteriano
+			file = open("images/Estufa.jpeg", "rb")
+			contenido = MIMEImage(file.read())
+			contenido.add_header('Content-Disposition', 'attachment; filename = "Estufa.jpeg"')
+			msg.attach(contenido)
+
+
+		#create server
+#		server = smtplib.SMTP('smtp.gmail.com: 587')
+
+#		server.starttls()
+
+		# Login Credentials for sending the mail
+#		server.login(msg['From'], password)
+
+
+		# send the message via the server.
+			server.sendmail(msg['From'], msg['To'], msg.as_string())
+
+#		server.quit()
+			sleep(30)
+#			print "successfully sent email to %s:" % (msg['To'])
+
+	server.quit()
+
+m.quit()
+#print "Finalizado"
